b54841c4ad212b28b25c9e5f48328ac10ba166ec
Nilesh091 <ranjannilesh40@gmail.com>
2026-01-17 10:07:37 +0530

    Completed 13th Jan assignment

---
+++ Assessments/Assessments/User Verification/Program.cs
@@ -0,0 +1,60 @@
+﻿
+using System;
+
+namespace UserVerificationApp
+{
+
+  public class InvalidPhoneNumberException : Exception
+  {
+    public InvalidPhoneNumberException(string message) : base(message)
+    {
+    }
+  }
+
+
+  class User
+  {
+    public string Name { get; set; }
+    public string PhoneNumber { get; set; }
+  }
+
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Program p = new Program();
+
+      Console.WriteLine("Enter Name:");
+      string name = Console.ReadLine();
+
+      Console.WriteLine("Enter Phone Number:");
+      string phoneNumber = Console.ReadLine();
+
+      try
+      {
+        User user = p.ValidatePhoneNumber(name, phoneNumber);
+        Console.WriteLine("Phone number verified successfully");
+      }
+      catch (InvalidPhoneNumberException ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+
+    public User ValidatePhoneNumber(string name, string phoneNumber)
+    {
+      if (phoneNumber.Length != 10)
+      {
+        throw new InvalidPhoneNumberException("Invalid phone number");
+      }
+
+      User user = new User
+      {
+        Name = name,
+        PhoneNumber = phoneNumber
+      };
+
+      return user;
+    }
+  }
+}
